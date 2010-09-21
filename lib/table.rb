babcaf0737507d150662e0ae133483fc82363633
tehviking <brandon.hays@gmail.com>
2010-09-21 00:35:21 -0600

    added some row operations. Table class is getting fat.

--- lib/table.rb
+++ lib/table.rb
@@ -12,8 +12,26 @@ class Table
     self.md_array = YAML::load(data_file)
   end
   
-  def return_row
-    @row = @md_array.each { |a| @rows << a[0] }
+  def return_row(ordinal)
+    self.md_array[ordinal]
+  end
+  
+  def append_row(row)
+    self.md_array << row
+  end
+  
+  def insert_row(row, ordinal)
+    self.md_array.insert(ordinal, row)
+  end
+
+  def return_column(ordinal)
+    column = []
+    self.md_array.each { |a| column << a[ordinal] }
+    return column
+  end
+  
+  def column_name(column)
+    @column[0]
   end
   
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,13 +1,6 @@
 require 'table'
 
 describe Table do
-  context "includes a file" do
-    it "creates an array" do
-      table = Table.new('/Users/bhays/Sites/s1-exam/data/s1-exam-data.yaml')
-      table.md_array.should be_an_instance_of(Array)
-      table.md_array[0][0].should == "PROCEDURE_DATE"
-    end
-  end
   
   context "doesn't include a file" do
     it "is initialized" do
@@ -16,4 +9,40 @@ describe Table do
     end
   end
   
+  context "includes a file" do
+    before(:each) do
+      @table = Table.new('/Users/bhays/Sites/s1-exam/data/s1-exam-data.yaml')      
+    end
+    
+    it "creates an array" do
+      @table.md_array.should be_an_instance_of(Array)
+      @table.md_array[0][0].should == "PROCEDURE_DATE"
+    end
+  
+    it "returns a row" do
+      @row = @table.return_row(0) 
+    
+      @row[0].should == "PROCEDURE_DATE"
+    end
+  
+    it "appends a row" do
+      new_row = ["05/31/06", "0", "7700", "0", "1888"]
+      @table.append_row(new_row)
+      
+      @table.md_array.last.should == ["05/31/06", "0", "7700", "0", "1888"]
+    end
+    
+    it "inserts a row" do
+      new_row = ["05/31/06", "0", "7700", "0", "1888"]
+      @table.insert_row(new_row, 1)
+      
+      @table.md_array[1].should == ["05/31/06", "0", "7700", "0", "1888"]
+    end
+  
+    it "returns a column" do
+      @column = @table.return_column(0) 
+    
+      @column[0].should == "PROCEDURE_DATE"
+    end
+  end
 end
